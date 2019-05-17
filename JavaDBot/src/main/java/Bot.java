34f31bd44359b2975e46eddbf275f6134feb555a
JyckoS <34590467+JyckoS@users.noreply.github.com>
2019-05-17 11:21:47 +0700

    Update Bot.java

--- JavaDBot/src/main/java/Bot.java
+++ JavaDBot/src/main/java/Bot.java
@@ -2,6 +2,12 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -12,9 +18,18 @@ public class Bot extends ListenerAdapter {
 		System.out.println("DonateCraft bot is ready!");
 	}
 	@Override
-	public void onMessageReceived(MessageReceivedEvent e) {
-		System.out.println("Someone sent a message.");
-		e.getChannel().sendMessage("wweeww");
-		System.out.println("" + e.getMessageId() + e.getMessage());
+	public void onMessageReceived(MessageReceivedEvent event) {
+		if (event.getAuthor().isBot()) return;
+        User author = event.getAuthor();
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+        MessageChannel channel = event.getChannel();
+        Member member = event.getMember();
+        String nickname = member.getNickname();
+        Role role = event.getGuild().getPublicRole();
+        TextChannel textChannel = event.getGuild().getTextChannelsByName("CHANNEL_NAME",true).get(0);
+        textChannel.sendMessage("MESSAGE").queue();
+        //that is the most needed part, I believe
 	}
 }
+
