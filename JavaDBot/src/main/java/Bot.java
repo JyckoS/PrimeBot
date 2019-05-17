182fe67c1453c696290e726712ea709a0ed232fe
JyckoS <34590467+JyckoS@users.noreply.github.com>
2019-05-17 11:13:58 +0700

    Update Bot.java

--- JavaDBot/src/main/java/Bot.java
+++ JavaDBot/src/main/java/Bot.java
@@ -7,11 +7,6 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Bot extends ListenerAdapter {
-
-	public static void main(String[] args) throws LoginException, InterruptedException {
-		// TODO Auto-generated method stub
-		JDA jda = new JDABuilder("NTc4NzYzMDIwOTYwNzkyNTc3.XN4VIQ.G_wiRoDsYj2_ttTGX2uriP5cLcU").addEventListener(new Bot()).build();
-	}
 	@Override
 	public void onReady(ReadyEvent e) {
 		System.out.println("DonateCraft bot is ready!");
